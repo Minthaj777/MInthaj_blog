079870cc23bd8f665012d097577f1e146a1f7328
minthaj <minthajsharafain@gmail.com>
2023-07-08 12:29:21 +0530

    first commit

---
+++ js/homepage.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".multiple-text", {
+    strings: [
+      "Frontend Developer",
+      "Graphics Designer",
+      "video Editor",
+      "software developer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
+});
